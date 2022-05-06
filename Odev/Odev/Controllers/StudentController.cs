0bfff552c5890b2f2fc02937ed320f57ff9bf34f
BesteSakar <bestesakarr@outlook.com>
2022-05-07 02:30:04 +0300

    Mvc-ExerciseProject1
    Posting process by receiving name, surname and e-mail from the user with student controller and limitations on these attributes. Also, sending data through the model in the controller.

---
+++ Odev/Odev/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Odev.Models;
+using System.Collections.Generic;
+
+namespace Odev.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult GetStudent()
+        {
+            return View();
+        }
+
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+               
+               
+            }
+
+            return View();
+           
+        }
+
+
+
+    public IActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student { StudentName = "Beste",StudentSurname="Sakar",StudentEmail="Beste@gmail.com"}
+            };
+            return View(students);
+        }
+    }
+}
---
+++ Odev/Odev/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Odev.Models
+{
+    public class Student
+    {
+
+
+
+        [Required(ErrorMessage = "Please enter a student name ..")]
+        [StringLength(40, ErrorMessage = "Must be no more than 50 characters ")]
+
+        public string StudentName { get; set; }
+
+        public string StudentSurname { get; set; }
+
+        [EmailAddress(ErrorMessage = "Please check your email structure..")]
+        public string StudentEmail { get; set; }
+
+
+
+    }
+}
